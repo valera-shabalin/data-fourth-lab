591c58618099a0cd3dd8d980a10d89ff48b8513a
Valera Shabalin <shabalinvalera.ts@gmail.com>
2020-12-03 00:18:13 +0700

    Add menu

--- fourth-lab.cpp
+++ fourth-lab.cpp
@@ -4,16 +4,85 @@
 
 using namespace std;
 
+int RandInt(int min, int max) 
+{
+    return min + rand() % (max - min);
+}
+
+void PrintMenu()
+{
+    system("cls");
+    cout << "Выберите действие: " << endl <<
+        "1. Заполнить дерево из файла;" << endl <<
+        "2. Заполнить дерево случайными данными;" << endl <<
+        "3. Заполнить дерево самостоятельно;" << endl <<
+        "4. Выход." << endl;
+    cout << ">";
+}
+
+int GetVariable() 
+{
+    int variable = 0;
+
+    cin >> variable;
+
+    while (variable < 1 || variable > 4) {
+        cout << "Неверное значение. Попробуйте ещё раз: ";
+        cin >> variable;
+    }
+
+    return variable;
+}
+
 int main() 
 {
     setlocale(LC_CTYPE, "Rus");
 
-    Tree t = Tree();
+    int variable = 0, x = 0;
+    size_t count = 0;
 
-    t.InsertNode(10).InsertNode(5).InsertNode(3).InsertNode(15).InsertNode(18).InsertNode(8).InsertNode(9);
+    do {
+        Tree tree = Tree();
 
-    cout << "Исходное дерево" << t;
+        PrintMenu();
 
-    cout << "Сумма нетерминальных узлов дерева равна " << t.GetNodesSumm() << endl;
+        variable = GetVariable();
+
+        switch (variable) 
+        {
+        case 1:
+            cout << "Заполняем дерево из файла.";
+            break;
+        case 2:
+            cout << "Введите нужное количество узлов в дереве: ";
+            cin >> count;
+
+            for (size_t i = 0; i < count; i++)
+            {
+                tree.InsertNode(RandInt(-10, 10));
+            }
+
+            cout << "Полученное дерево: " << tree;
+            cout << "Сумма не терминальный узлов: " << tree.GetNodesSumm() << endl;
+            system("pause");
+            break;
+        case 3:
+            cout << "Заполняйте дерево. Введите -1, чтобы закончить: " << endl;
+
+            do
+            {
+                cout << ">";
+                cin >> x;
+                tree.InsertNode(x);
+            } while (x != -1);
+
+            cout << "Полученное дерево: " << tree;
+            cout << "Сумма не терминальный узлов: " << tree.GetNodesSumm() << endl;
+            system("pause");
+            break;
+        }
+    } while (variable != 4);
+
+    return 0;
 }
 
